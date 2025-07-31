1b1f80dbeaae1c4673327c6425cf55b5e3bee844
FabricioEllerSL <fabricioellersl@gmail.com>
2025-07-31 15:12:52 -0300

    :wrench: feat: middlewares config

--- index.js
+++ index.js
@@ -6,6 +6,57 @@ const FileStore = require('session-file-store')(session);
 const flash = require('express-flash');
 
 const app = express();
+const port = process.env.PORT
 
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs.engine({extname: 'hbs'}));
+app.set('view engine', 'hbs');
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+app.use(session({
+    name: "session",
+    secret: "our_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+        logFn: ()=>{},
+        path: require('path').join(require('os').tmpdir(), 'sessions')
+    }),
+    cookie: {
+        secure: false,
+        maxAge: 360000,
+        expires: new Date(Date.now() + 360000),
+        httpOnly: true
+    }
+}))
+
+app.use(flash())
+
+app.use(express.static('public'))
+
+app.use((req, res, next) => {
+    if(req.session.userid){
+        res.locals.session = req.session
+    }
+
+    next()
+})
+
+const conn = require('./db/conn')
+
+conn.sync().then(
+    () => {
+        app.listen(port)
+        console.log(`App listening on port ${port}`)
+    }
+).catch(
+    (err) => {
+        console.log(err)
+    }
+)
