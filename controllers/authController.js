6d26c349be48ffa5e9df956589affa7b1a3dbf92
FabricioEllerSL <fabricioellersl@gmail.com>
2025-08-02 17:53:42 -0300

    :sparkles: feat: added post user and init session

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,10 +12,48 @@ module.exports = class AuthController{
 
     static async registerPost(req, res){
 
-        const name = req.body.name
-        const email = req.body.email
-        const password = req.body.password
+        const { name, email, password, confirmPassword } = req.body
 
+        // password match validation
+
+        if(password != confirmPassword){
+            req.flash("message", "Password and Confirmation don't match!!!")
+            res.render('auth/register')
+            return
+        }
+
+        // check if user exists
+
+        const checkIfUserExists = await User.findOne({where: {email: email}})
+
+        if(checkIfUserExists){
+            req.flash("message", "Email already in use!!!")
+            res.render('auth/register')
+            return
+        }
+
+        // creating passwds
+
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name, email, password: hashedPassword
+        }
+
+        try{
+
+            // init session
+            const createdUser = await User.create(user)
+            req.session.userid = createdUser.id
+            req.flash("message", "User successfully registered")
+            req.session.save(() => {
+                res.redirect('/opinion')
+            })
+
+        } catch(err){
+            console.log(`Error during create user: ${err}`)
+        }
         
 
     }
