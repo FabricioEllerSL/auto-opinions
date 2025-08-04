037a3061979d0f4560b320c8e6cf9d5e02d2a97b
FabricioEllerSL <fabricioellersl@gmail.com>
2025-08-04 11:39:31 -0300

    :sparkles: feat: added edit opinion funcionality

--- controllers/OpinionController.js
+++ controllers/OpinionController.js
@@ -14,13 +14,44 @@ module.exports = class OpinionController{
 
         const opinions = user.Opinions.map((result) => result.dataValues)
 
-        console.log(opinions)
+        let emptyOpinions = false
 
-        res.render('opinion/dashboard', {opinions})
+        if(opinions.length === 0){
+            emptyOpinions = true
+        }
+
+        res.render('opinion/dashboard', {opinions, emptyOpinions})
     }
 
     static addOpinion(req, res){
         res.render('opinion/create')
+    }
+
+    static async editOpinion(req, res){
+
+        const opinionId = req.params.id
+        
+        const opinion = await Opinion.findOne({where: {id:opinionId}, raw:true})
+
+        res.render('opinion/edit', {opinion})
+    }
+
+    static async editOpinionPost(req, res){
+        
+        const opinionId = req.body.id
+
+        const opinion = {
+            title: req.body.title
+        }
+
+        try{
+            await Opinion.update(opinion, {where: {id:opinionId}})
+            req.session.save(()=>{
+                res.redirect('/opinion/dashboard')
+            })
+        } catch(err){
+            console.log(`Error during edit opinion: ${err}`)
+        }
     }
 
     static async addOpinionPost(req, res){
@@ -40,7 +71,20 @@ module.exports = class OpinionController{
         } catch(err){
             console.log(`Error during create opinion: ${err}`)
         }
+    }
 
+    static async removeOpinion(req, res){
+        const id = req.body.id
+        const userId = req.session.userid
 
+        try{
+            await Opinion.destroy({where: {id: id, UserId: userId}})
+            req.flash('message', 'Opinion Successfully deleted!')
+            req.session.save(() => {
+                res.redirect('/opinion/dashboard')
+            })
+        } catch(err){
+            console.log(`Error during delete opinion: ${err}`)
+        }
     }
 }
--- routes/opinionRoutes.js
+++ routes/opinionRoutes.js
@@ -9,5 +9,8 @@ router.get('/', OpinionController.showOpinions)
 router.get('/dashboard', checkAuth, OpinionController.dashboard)
 router.get('/add', checkAuth, OpinionController.addOpinion)
 router.post('/add', checkAuth, OpinionController.addOpinionPost)
+router.get('/edit/:id', checkAuth, OpinionController.editOpinion)
+router.post('/edit', checkAuth, OpinionController.editOpinionPost)
+router.post('/remove', checkAuth, OpinionController.removeOpinion)
 
 module.exports = router
