0a0c8c7c5ae89077bd3cf7568c0177beb5ff24ee
FabricioEllerSL <fabricioellersl@gmail.com>
2025-08-04 10:44:07 -0300

    :sparkles: feat: helper for checking if user is logged

--- routes/opinionRoutes.js
+++ routes/opinionRoutes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const OpinionController = require('../controllers/OpinionController')
 
+// helper
+const checkAuth = require('../helpers/auth').checkAuth
+
 router.get('/', OpinionController.showOpinions)
-router.get('/dashboard', OpinionController.dashboard)
+router.get('/dashboard', checkAuth, OpinionController.dashboard)
 
 module.exports = router
